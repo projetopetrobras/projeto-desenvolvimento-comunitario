000c8697c9cca577f34dd4115a38c4e7e067c78e
petrobrasprojeto <itsmechrisbb@gmail.com>
2024-11-28 16:59:53 -0300

    Add files via upload

---
+++ tcc/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section, div[id]');
+    const navLinks = document.querySelectorAll('.menu li a');
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(currentSection)) {
+            link.classList.add('active');
+        }
+    });
+});
